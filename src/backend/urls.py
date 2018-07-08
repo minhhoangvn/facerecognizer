6e1bf38207236d8415ff529c7b84867ad37a40cb
minh hoang <minhhoang@minhhoang.local>
2018-07-08 23:38:12 +0700

    Update uwsgi config file

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -5,5 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
+    url(r'test', TemplateView.as_view(template_name="base.html")),
+    url(r'sample', TemplateView.as_view(template_name="index.html")),
+    url(r'register', TemplateView.as_view(template_name="register/index.html")),
 ]
