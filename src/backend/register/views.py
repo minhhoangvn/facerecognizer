b115cd6080e00f0ed0209cd769f0da49a453c803
minh hoang <minhhoang@minhhoang.local>
2018-07-13 00:26:04 +0700

    Update signup model

--- src/backend/register/views.py
+++ src/backend/register/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from .serializers import UserProfileSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status
 
 
@@ -13,20 +14,19 @@ class UserList(APIView):
     List all users, or create a new user.
     """
 
-    def get(self, request, format=None):
-
+    def get(self, request: Request, format=None):
         users = UserProfile.objects.all()
         serializer = UserProfileSerializer(users, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = UserProfileSerializer(data=request.DATA)
+    def post(self, request: Request, format=None):
+        serializer = UserProfileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
+    def delete(self, request: Request, pk, format=None):
         user = self.get_object(pk)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -43,14 +43,14 @@ class UserDetail(APIView):
         except UserProfile.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
+    def get(self, request: Request, pk, format=None):
         user = self.get_object(pk)
         user = UserProfileSerializer(user)
         return Response(user.data)
 
-    def put(self, request, pk, format=None):
+    def put(self, request: Request, pk, format=None):
         user = self.get_object(pk)
-        serializer = UserProfileSerializer(user, data=request.DATA)
+        serializer = UserProfileSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
