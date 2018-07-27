f20c1dfa125af90aa10826072b2f5e8c8c66857d
minh hoang <minhhoang@minhhoang.local>
2018-07-27 23:24:57 +0700

    Register form

--- src/frontend/src/containers/register/Register.js
+++ src/frontend/src/containers/register/Register.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
+import {
+  Grid,
+  GridList,
+  GridListTile,
+  GridListTileBar
+} from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceICon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+
+/**
+ * Register Page
+ */
 const styles = theme => ({
   container: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    fontSize: '1.0em'
   },
   button: {
     margin: theme.spacing.unit,
-    width: 100,
-    backgroundColor: '#37cc57',
-    fontSize: '0.8em',
-    fontWeight: 900,
-    color: '#1b2363'
+    width: 200
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200
+    width: 200,
+    fontSize: '0.8em'
   },
   menu: {
+    fontSize: '0.8em',
     width: 200
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  },
+  iconSmall: {
+    fontSize: 20
+  },
+  gridLayout: {
+    marginLeft: '30%',
+    padding: 10
   }
 });
 
@@ -38,77 +66,85 @@ class RegisterForm extends React.Component {
 
   render() {
     const { classes } = this.props;
-
     return (
-      <form className={classes.container} noValidate autoComplete="off">
-        <TextField
-          required
-          id="user-input"
-          label="UserName"
-          className={classes.textField}
-          autoComplete="username"
-          margin="normal"
-          placeholder="Input your username"
-        />
-        <TextField
-          required
-          id="password-input"
-          label="Password"
-          className={classes.textField}
-          type="password"
-          autoComplete="current-password"
-          margin="normal"
-          placeholder="Input your password"
-        />
-        <TextField
-          required
-          id="email-input"
-          label="Email"
-          className={classes.textField}
-          type="email"
-          autoComplete="email"
-          margin="normal"
-          placeholder="Input your email"
-        />
-        <TextField
-          required
-          id="firstname-input"
-          label="First Name"
-          className={classes.textField}
-          type="text"
-          autoComplete="given-name"
-          margin="normal"
-          placeholder="Input your first-name"
-        />
-        <TextField
-          required
-          id="lastname-input"
-          label="Last Name"
-          className={classes.textField}
-          type="text"
-          autoComplete="family-name"
-          margin="normal"
-          placeholder="Input your last-name"
-        />
-        <TextField
-          id="location-input"
-          label="Location"
-          className={classes.textField}
-          type="text"
-          margin="normal"
-          placeholder="Input your location"
-        />
-        <TextField
-          id="birth-input"
-          label="Birthday"
-          className={classes.textField}
-          type="text"
-          margin="normal"
-          placeholder="Input your birthday"
-        />
-        <Button variant="flat" size="large" className={classes.button}>
-          Register Account
-        </Button>
+      <form className={classes.container} autoComplete="off">
+        <Grid
+          className={classes.gridLayout}
+          item
+          xs={4}
+          spacing={8}
+          alignContent={'space-around'}
+          container={true}
+        >
+          <TextField
+            required
+            id="user-input"
+            label="UserName"
+            className={classes.textField}
+            autoComplete="username"
+            margin="normal"
+            placeholder="Input your username"
+          />
+          <TextField
+            required
+            id="password-input"
+            label="Password"
+            className={classes.textField}
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            placeholder="Input your password"
+          />
+          <TextField
+            required
+            id="email-input"
+            label="Email"
+            className={classes.textField}
+            type="email"
+            autoComplete="email"
+            margin="normal"
+            placeholder="Input your email"
+          />
+          <TextField
+            required
+            id="firstname-input"
+            label="First Name"
+            className={classes.textField}
+            type="text"
+            autoComplete="given-name"
+            margin="normal"
+            placeholder="Input your first-name"
+          />
+          <TextField
+            required
+            id="lastname-input"
+            label="Last Name"
+            className={classes.textField}
+            type="text"
+            autoComplete="family-name"
+            margin="normal"
+            placeholder="Input your last-name"
+          />
+          <TextField
+            id="location-input"
+            label="Location"
+            className={classes.textField}
+            type="text"
+            margin="normal"
+            placeholder="Input your location"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            size="small"
+            className={classes.button}
+          >
+            <SaveIcon
+              className={classNames(classes.leftIcon, classes.iconSmall)}
+            />
+            Register Account
+          </Button>
+        </Grid>
       </form>
     );
   }
